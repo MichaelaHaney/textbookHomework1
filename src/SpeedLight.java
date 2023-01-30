53d65d7bb7a084fd1a0726427793c2ef2db58010
Michaela Haney <mhaney3@ggc.edu>
2023-01-30 14:23:52 -0500

    added commented line

---
+++ src/SpeedLight.java
@@ -0,0 +1,16 @@
+/**
+ * @Class: SpeedLight
+ * @Author: Michaela Haney
+ * @Version: 1.0
+ * @Date: 30 January 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+
+
+public class SpeedLight {
+    public static void main (String[]args){
+    //complete here
+    }
+}
