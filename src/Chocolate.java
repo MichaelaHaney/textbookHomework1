40d7eea2a8b16c7172a2719caf7e3d02584f179f
Michaela Haney <mhaney3@ggc.edu>
2023-01-30 22:30:42 -0500

    add Chocolate.java

---
+++ src/Chocolate.java
@@ -0,0 +1,12 @@
+/**
+ * @Class: Chocolate
+ * @Author: Michaela Haney
+ * @Version: 1.0
+ * @Course: ITEC 2140 Section 05 Spring 2023
+ * @Written: 30 January 2023
+ *
+ * Description - Chocolate
+ */
+
+public class Chocolate {
+}
