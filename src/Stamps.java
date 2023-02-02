3559d830c4c55cc8cc80249742ff85359f18dbc5
Michaela Haney <mhaney3@ggc.edu>
2023-02-02 17:50:05 -0500

    added difference = j - s;

--- src/Stamps.java
+++ src/Stamps.java
@@ -4,11 +4,19 @@
  * @Version: 1.0
  * @Course: ITEC 2140 Spring 2023
  * @Written: 30 January 2023
- * Description - Stamps
+ * Description - Write a program that
+ * calculates the difference
+ * between Jean's and Susan's collection
+ * of stamps.
  */
 
 public class Stamps {
     public static void main (String[]args){
+        int s = 8;
+        int j = 40;
+        int difference;
+        difference = j - s;
+        System.out.println(j-s);
 
     }
 }
