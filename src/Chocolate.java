4d68b0cc545b54d1d31fe6d0c6c0bb8f74e8a0c5
Michaela Haney <mhaney3@ggc.edu>
2023-02-02 13:17:31 -0500

    added int tmp = a;

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -2,14 +2,21 @@
  * @Class: Chocolate
  * @Author: Michaela Haney
  * @Version: 1.0
- * @Course: ITEC 2140 Section 05 Spring 2023
- * @Written: 30 January 2023
+ * @Course: ITEC 2140 - 05, Spring 2023
+ * @Written: 31 January 2023
  *
- * Description - Chocolate
+ * Description - Write a program that
+ * will put 3 bags of
+ * chocolate instead of 2
+ * bags of chocolate.
  */
 
 public class Chocolate {
     public static void main (String[]args) {
-
+    int a = 2;
+    int b = 3;
+    int tmp = a;
+    a = b;
+    System.out.println(a);
     }
 }
