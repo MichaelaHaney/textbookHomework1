517a7bb21b90590b06f22b6359460f7106101ca7
Michaela Haney <mhaney3@ggc.edu>
2023-02-02 19:33:52 -0500

    added femaleStudents = 175;

---
+++ src/MaleStudent.java
@@ -0,0 +1,18 @@
+/**
+ * @Class: MaleStudent
+ * @Author: Michaela Haney
+ * @Version: 1.0
+ * @Course: ITEC 2140- 05, Spring 2023
+ * @Written: 2 February 2023
+ * Description: Write a program that calculates how many
+ * male students are in the middle school.
+ */
+
+public class MaleStudent {
+    public static void main (String []args){
+        int totalStudents = 389;
+        int femaleStudents = 175;
+        int difference = totalStudents - femaleStudents;
+        System.out.println(difference);
+    }
+}
