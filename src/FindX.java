38004dc995bc037dcf3ac6f040d8a2045bc1fc75
Michaela Haney <mhaney3@ggc.edu>
2023-02-02 19:07:25 -0500

    added //194-71=123;

---
+++ src/FindX.java
@@ -0,0 +1,19 @@
+/**
+ * @Class: FindX
+ * @Author: Michaela Haney
+ * @Version: 1.0
+ * @Course: ITEC 2140 - 005, Spring 2023
+ * @Written: 2 February 2023
+ * Description: Write a program that calculates the unknown
+ * number based on the following formula: 5
+ * + 19 + X + 47 = 194.
+ */
+
+public class FindX {
+    public static void main (String []args){
+        //5 + 19 + 47 = 71;
+        //194-71=123;
+        int sum = 123+71;
+        System.out.println(sum);
+    }
+}
